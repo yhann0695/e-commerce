18acdd15dc035eb8a9efd0747d5aee71484b9329
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-24 23:06:59 -0300

    Teste de inicio do app.js

--- auth-api/app.js
+++ auth-api/app.js
@@ -4,6 +4,14 @@ const app = express();
 const env = process.env;
 const PORT = env.PORT || 8080;
 
+app.get('/api/status', (req, res) => {
+    return res.status(200).json({
+        service: 'Auth-API',
+        status: 'ok',
+        httpStatus: 200 
+    });
+})
+
 app.listen(PORT, () => { 
     console.info(`Server started successfully at port ${PORT}`);
 });

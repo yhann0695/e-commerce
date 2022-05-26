997d215bdf0d7a99d5ad5815b806d807fe6aca6c
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-26 18:10:54 -0300

    Criação do projeto java com Gradle

--- product-api/src/main/java/br/com/product/api/productapi/ProductApiApplication.java
+++ product-api/src/main/java/br/com/product/api/productapi/ProductApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.product.api;
+package br.com.product.api.productapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- product-api/src/test/java/br/com/product/api/productapi/ProductApiApplicationTests.java
+++ product-api/src/test/java/br/com/product/api/productapi/ProductApiApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.product.api;
+package br.com.product.api.productapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;

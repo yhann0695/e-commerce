2877d9e7db219275a7ee26270b8a7c7c583cc777
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-28 16:06:11 -0300

    exposing to end point to access token

--- auth-api/src/modules/user/controller/UserController.js
+++ auth-api/src/modules/user/controller/UserController.js
@@ -2,6 +2,11 @@ import UserService from '../service/UserService.js';
 
 class UserController {
 
+    async getAccessToken(request, response) {
+        let accessToken = await UserService.getAccessToken(request);
+        return response.status(accessToken.status).json(accessToken);
+    }
+
     async findByEmail(request, response) {
         let user = await UserService.findByEmail(request);
         return response.status(user.status).json(user);

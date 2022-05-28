f510b393674b8d2c45bbab23fb0d215353f8dacb
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-28 12:21:35 -0300

    creating token for access

--- auth-api/src/utility.js
+++ auth-api/src/utility.js
@@ -1,5 +1,11 @@
+const env = process.env;
+
 export const SUCCESS = 200;
 export const BAD_REQUEST = 400;
 export const UNAUTHORIZED = 401;
 export const FORBIDDEN = 403;
 export const INTERVAL_SERVER_ERROR = 500;
+
+export const apiSecret = env.API_SECRET 
+    ? env.API_SECRET 
+    : 'YXV0aC1hcGktc2VjcmV0LWRldi0xMjM0NTY=';

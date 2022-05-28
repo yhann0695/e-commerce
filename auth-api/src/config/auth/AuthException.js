327a06a4f287f7efdc6be59d612da573c2b4bd5b
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-28 16:58:29 -0300

    creating Auth Exception class

---
+++ auth-api/src/config/auth/AuthException.js
@@ -0,0 +1,11 @@
+class AuthException extends Error {
+    constructor(status, message) {
+        super(message);
+        this.status = status;
+        this.message = message;
+        this.name = this.constructor.name;
+        Erro.captureStackTrace(this.this.constructor);
+    }
+}
+
+export default AuthException;

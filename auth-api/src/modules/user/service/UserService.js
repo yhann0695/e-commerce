297de4588feac89ea25f92a04d45f7142648414a
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-28 16:03:08 -0300

    creating function accessToken

--- auth-api/src/modules/user/service/UserService.js
+++ auth-api/src/modules/user/service/UserService.js
@@ -1,3 +1,6 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 import UserRepository from '../repository/UserRepository.js';
 import * as utility from '../../../utility.js';
 import UserException from '../exception/UserException.js';
@@ -19,7 +22,7 @@ class UserService {
                 }
             }
         } catch (err) {
-            return {
+            return { 
                 status: err.status ? err.status : utility.INTERVAL_SERVER_ERROR,
                 message: err.status
             }
@@ -35,6 +38,37 @@ class UserService {
     validateUserNotFound(user) {
         if(!user) {
             throw new UserException(utility.BAD_REQUEST, "User was not found.");
+        }
+    }
+
+    async getAccessToken(req) {
+        try {
+            const { email, password } = req.body;
+            this.validateAccessTokenData(email, password);
+            let user = await UserRepository.findByEmail(email);
+            this.validateUserNotFound(user);
+            await this.validatePassword(password, user.password);
+            const authUser = { id: user.id, name: user.name, email: user.email };
+            const accessToken = jwt.sign({ authUser }, utility.API_SECRET, { expiresIn: "1d" });
+            return { status: utility.SUCCESS, accessToken }
+        } catch (err) {
+            return { 
+                status: err.status ? err.status : utility.INTERVAL_SERVER_ERROR,
+                message: err.status
+            }
+        }
+
+    }
+
+    validateAccessTokenData(email, password) {
+        if(!email || !password) {
+            throw new UserException(utility.UNAUTHORIZED, "Email and password must be in informed.");
+        }
+    }
+
+    async validatePassword(password, hashPassword) {
+        if(!await bcrypt.compare(password, hashPassword)) {
+            throw new UserException(utility.UNAUTHORIZED, "Password doesn't match.");
         }
     }
 }

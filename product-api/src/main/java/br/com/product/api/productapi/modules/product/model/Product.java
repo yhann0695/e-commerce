8bb1525e3d103da07eee6808c4aced1a669d9582
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-29 14:26:51 -0300

    creating Product class

---
+++ product-api/src/main/java/br/com/product/api/productapi/modules/product/model/Product.java
@@ -0,0 +1,23 @@
+package br.com.product.api.productapi.modules.product.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Integer id;
+
+    @Column(name = "PRODUCT_NAME", nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "FK_CATEGORY", nullable = false)
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "FK_SUPPLIER", nullable = false)
+    private Supplier supplier;
+}

d0deb929bdcaf5dea57b83c6efe6a74e07f23d4b
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-29 20:08:40 -0300

    implementation of request and response to category

--- product-api/src/main/java/br/com/product/api/productapi/modules/product/dto/category/CategoryRequest.java
+++ product-api/src/main/java/br/com/product/api/productapi/modules/product/dto/category/CategoryRequest.java
@@ -1,4 +1,11 @@
 package br.com.product.api.productapi.modules.product.dto.category;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class CategoryRequest {
+
+    @Setter
+    @Getter
+    private String description;
 }
--- product-api/src/main/java/br/com/product/api/productapi/modules/product/dto/category/CategoryResponse.java
+++ product-api/src/main/java/br/com/product/api/productapi/modules/product/dto/category/CategoryResponse.java
@@ -1,4 +1,23 @@
 package br.com.product.api.productapi.modules.product.dto.category;
 
+import br.com.product.api.productapi.modules.product.model.Category;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
 public class CategoryResponse {
+
+    @Setter
+    @Getter
+    private Integer id;
+
+    @Setter
+    @Getter
+    private String description;
+
+    public static CategoryResponse of(Category category) {
+        var response = new CategoryResponse();
+        BeanUtils.copyProperties(category, response);
+        return response;
+    }
 }

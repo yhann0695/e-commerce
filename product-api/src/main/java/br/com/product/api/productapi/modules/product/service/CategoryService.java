d4a5514c59fbab27b88a8feec78534baf7fec920
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-29 20:10:23 -0300

    implementation of method create category

--- product-api/src/main/java/br/com/product/api/productapi/modules/product/service/CategoryService.java
+++ product-api/src/main/java/br/com/product/api/productapi/modules/product/service/CategoryService.java
@@ -1,5 +1,8 @@
 package br.com.product.api.productapi.modules.product.service;
 
+import br.com.product.api.productapi.modules.product.dto.category.CategoryRequest;
+import br.com.product.api.productapi.modules.product.dto.category.CategoryResponse;
+import br.com.product.api.productapi.modules.product.model.Category;
 import br.com.product.api.productapi.modules.product.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -9,4 +12,9 @@ import org.springframework.stereotype.Service;
 public class CategoryService {
 
     private CategoryRepository categoryRepository;
+
+    public CategoryResponse create(CategoryRequest request) {
+        var entity= categoryRepository.save(Category.of(request));
+        return CategoryResponse.of(entity);
+    }
 }

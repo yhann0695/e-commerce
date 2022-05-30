3cbe3c66f8fbd8b93554f3ddbfa62a94cba0b264
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-30 11:06:45 -0300

    creating method for convert entity

--- product-api/src/main/java/br/com/product/api/productapi/modules/supplier/model/Supplier.java
+++ product-api/src/main/java/br/com/product/api/productapi/modules/supplier/model/Supplier.java
@@ -1,9 +1,16 @@
 package br.com.product.api.productapi.modules.supplier.model;
 
+import br.com.product.api.productapi.modules.supplier.dto.SupplierRequest;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "SUPPLIER")
+@Setter
+@Getter
 public class Supplier {
 
     @Id
@@ -12,4 +19,10 @@ public class Supplier {
 
     @Column(name = "SUPPLIER_NAME", nullable = false)
     private String name;
+
+    public static Supplier of(SupplierRequest request) {
+        var entity = new Supplier();
+        BeanUtils.copyProperties(request, entity);
+        return entity;
+    }
 }

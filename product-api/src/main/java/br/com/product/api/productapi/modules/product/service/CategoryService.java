6b977351fa8d2a45fc2662daf23bb68b9e063238
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-29 21:20:08 -0300

    creating method of validation in category service

--- product-api/src/main/java/br/com/product/api/productapi/modules/product/service/CategoryService.java
+++ product-api/src/main/java/br/com/product/api/productapi/modules/product/service/CategoryService.java
@@ -1,11 +1,14 @@
 package br.com.product.api.productapi.modules.product.service;
 
+import br.com.product.api.productapi.configuration.exception.ValidationException;
 import br.com.product.api.productapi.modules.product.dto.category.CategoryRequest;
 import br.com.product.api.productapi.modules.product.dto.category.CategoryResponse;
 import br.com.product.api.productapi.modules.product.model.Category;
 import br.com.product.api.productapi.modules.product.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Service
 @AllArgsConstructor
@@ -14,7 +17,14 @@ public class CategoryService {
     private CategoryRepository categoryRepository;
 
     public CategoryResponse create(CategoryRequest request) {
+        validateCategoryNameInformed(request);
         var entity= categoryRepository.save(Category.of(request));
         return CategoryResponse.of(entity);
+    }
+
+    private void validateCategoryNameInformed(CategoryRequest request) {
+        if(isEmpty(request.getDescription())) {
+            throw new ValidationException("The category description was not informed.");
+        }
     }
 }

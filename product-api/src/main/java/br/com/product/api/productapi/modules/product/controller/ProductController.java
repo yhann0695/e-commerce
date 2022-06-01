ab82786f2cdb981500f9203319b612859a9b1c40
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-06-01 00:23:02 -0300

    creating methods of getting product with name, id and getting all

--- product-api/src/main/java/br/com/product/api/productapi/modules/product/controller/ProductController.java
+++ product-api/src/main/java/br/com/product/api/productapi/modules/product/controller/ProductController.java
@@ -3,21 +3,45 @@ package br.com.product.api.productapi.modules.product.controller;
 import br.com.product.api.productapi.modules.product.dto.ProductRequest;
 import br.com.product.api.productapi.modules.product.dto.ProductResponse;
 import br.com.product.api.productapi.modules.product.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
+@AllArgsConstructor
 public class ProductController {
 
-    @Autowired
     private ProductService productService;
 
     @PostMapping
     public ProductResponse create(@RequestBody ProductRequest request) {
         return productService.create(request);
+    }
+
+    @GetMapping
+    public List<ProductResponse> findAll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/id/{id}")
+    public ProductResponse findById(@PathVariable Integer id) {
+        return productService.findByIdResponse(id);
+    }
+
+    @GetMapping("/name/{name}")
+    public List<ProductResponse> findByName(@PathVariable String name) {
+        return productService.findByName(name);
+    }
+
+    @GetMapping("/supplierId/{supplierId}")
+    public List<ProductResponse> findBySupplierId(@PathVariable Integer supplierId) {
+        return productService.findBySupplierId(supplierId);
+    }
+
+    @GetMapping("/categoryId/{categoryId}")
+    public List<ProductResponse> findByCategoryId(@PathVariable Integer categoryId) {
+        return productService.findBySupplierId(categoryId);
     }
 }
--- product-api/src/main/java/br/com/product/api/productapi/modules/product/service/ProductService.java
+++ product-api/src/main/java/br/com/product/api/productapi/modules/product/service/ProductService.java
@@ -1,5 +1,6 @@
 package br.com.product.api.productapi.modules.product.service;
 
+import br.com.product.api.productapi.configuration.exception.ValidationException;
 import br.com.product.api.productapi.modules.category.model.Category;
 import br.com.product.api.productapi.modules.category.service.CategoryService;
 import br.com.product.api.productapi.modules.product.dto.ProductRequest;
@@ -13,6 +14,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
 
 @Service
 @AllArgsConstructor
@@ -31,6 +35,10 @@ public class ProductService {
         return ProductResponse.of(product);
     }
 
+    private void validate(ProductRequest request) {
+        this.validations.forEach(v -> v.validate(request));
+    }
+
     private Supplier getSupplier(ProductRequest request) {
         return supplierService.findById(request.getSupplierId());
     }
@@ -39,9 +47,37 @@ public class ProductService {
         return categoryService.findById(request.getCategoryId());
     }
 
-    private void validate(ProductRequest request) {
-        this.validations.forEach(v -> v.validate(request));
+    public List<ProductResponse> findAll() {
+        return productRepository.findAll().stream().map(ProductResponse::of).collect(Collectors.toList());
     }
 
+    public List<ProductResponse> findByName(String name) {
+        if(isEmpty(name)) {
+            throw new ValidationException("The product name must be informed.");
+        }
+        return productRepository.findByNameIgnoreCaseContaining(name).stream().map(ProductResponse::of).collect(Collectors.toList());
+    }
+
+    public List<ProductResponse> findBySupplierId(Integer supplierId) {
+        if(isEmpty(supplierId)) {
+            throw new ValidationException("The product supplier ID name must be informed.");
+        }
+        return productRepository.findBySupplierId(supplierId).stream().map(ProductResponse::of).collect(Collectors.toList());
+    }
+
+    public ProductResponse findByIdResponse(Integer id) {
+        return ProductResponse.of(findById(id));
+    }
+
+    private Product findById(Integer id) {
+        return productRepository.findById(id).orElseThrow(() -> new ValidationException("There's no product for the given ID."));
+    }
+
+    public List<ProductResponse> findByCategoryId(Integer categoryId) {
+        if(isEmpty(categoryId)) {
+            throw new ValidationException("The product category ID name must be informed.");
+        }
+        return productRepository.findByCategoryId(categoryId).stream().map(ProductResponse::of).collect(Collectors.toList());
+    }
 
 }

1b0a76647004f5b4dfe23a6e54a601d86d2a2f87
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-31 22:46:50 -0300

    Product query by name

--- product-api/src/main/java/br/com/product/api/productapi/modules/product/repository/ProductRepository.java
+++ product-api/src/main/java/br/com/product/api/productapi/modules/product/repository/ProductRepository.java
@@ -4,6 +4,10 @@ import br.com.product.api.productapi.modules.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    List<Product> findByNameIgnoreCaseContaining(String name);
 }

e3d92d2c4b55fb278eb0b17327f5cb180019f525
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-31 21:57:41 -0300

    creating method of getting category with description

--- product-api/src/main/java/br/com/product/api/productapi/modules/category/repository/CategoryRepository.java
+++ product-api/src/main/java/br/com/product/api/productapi/modules/category/repository/CategoryRepository.java
@@ -4,7 +4,10 @@ import br.com.product.api.productapi.modules.category.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
 
+    List<Category> findByDescriptionIgnoreCaseContaining(String description);
 }

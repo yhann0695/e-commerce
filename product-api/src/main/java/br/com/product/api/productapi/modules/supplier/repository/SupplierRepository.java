a4f6867acdc81bab28bf1493ac4029e5b18292e6
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-05-31 22:41:46 -0300

    supplier query by name

--- product-api/src/main/java/br/com/product/api/productapi/modules/supplier/repository/SupplierRepository.java
+++ product-api/src/main/java/br/com/product/api/productapi/modules/supplier/repository/SupplierRepository.java
@@ -4,6 +4,10 @@ import br.com.product.api.productapi.modules.supplier.model.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+
+    List<Supplier> findByNameIgnoreCaseContaining(String name);
 }

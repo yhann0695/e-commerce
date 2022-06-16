adb7e153a57208b6247b4356c5c4613632480a81
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-06-16 20:42:26 -0300

    class SalesClient to findProductSales in feignClient

---
+++ product-api/src/main/java/br/com/product/api/productapi/modules/product/sales/client/SalesClient.java
@@ -0,0 +1,15 @@
+package br.com.product.api.productapi.modules.product.sales.client;
+
+import br.com.product.api.productapi.modules.product.sales.dto.SalesProductResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@FeignClient(name = "salesClient", contextId = "salesClient", url = "${app-config.services.sales}")
+public interface SalesClient {
+
+    @GetMapping("/product/{productId}")
+    Optional<SalesProductResponse> findSalesByProductId(@PathVariable Integer productId);
+}

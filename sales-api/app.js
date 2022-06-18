3a795aa897d4a1466fe52dca8f468ad44346e2ec
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-06-17 21:31:28 -0300

    import to checkToken

--- sales-api/app.js
+++ sales-api/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
 
 import { connect } from "./src/config/db/mongoDbConfig.js";
-import { createInicialData } from './src/config/db/inicialData.js';
+import { createInicialData } from './src/config/db/initialData.js';
+import checkToken from './src/config/auth/checkToken.js';
  
 const app = express();
 const env = process.env;
@@ -9,6 +10,8 @@ const PORT = env.PORT || 8082;
 
 connect();
 createInicialData();
+
+app.use(checkToken);
 
 app.get('/api/status', async (req, res) => {
     return res.status(200).json({

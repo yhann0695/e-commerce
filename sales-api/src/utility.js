b281611b4cefeb2415df750dacab4400978a514a
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-06-25 12:06:36 -0300

    url product api

--- sales-api/src/utility.js
+++ sales-api/src/utility.js
@@ -11,3 +11,5 @@ export const API_SECRET = env.API_SECRET
     : 'YXV0aC1hcGktc2VjcmV0LWRldi0xMjM0NTY=';
 
 export const RABBIT_MQ_URL = env.RABBIT_MQ_URL ? env.RABBIT_MQ_URL : 'amqp://localhost:5672';
+
+export const PRODUCT_API_URL = env.PRODUCT_API_URL ? env.PRODUCT_API_URL : 'http://localhost:8081/api/product';

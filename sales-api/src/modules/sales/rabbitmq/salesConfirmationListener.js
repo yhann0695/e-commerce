f2c0e9ccd69bdb4422dbde59393b75b413dbab5f
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-06-25 12:05:48 -0300

    confirmation of sales

--- sales-api/src/modules/sales/rabbitmq/salesConfirmationListener.js
+++ sales-api/src/modules/sales/rabbitmq/salesConfirmationListener.js
@@ -1,8 +1,8 @@
 import amqp from 'amqplib/callback_api.js';
 
 import { RABBIT_MQ_URL } from '../../../utility.js';
-
 import { SALES_CONFIRMATION_QUEUE } from '../../../config/rabbitmq/queue.js';
+import OrderService from '../service/OrderService.js';
 
 export function listenToSalesConfirmationQueue() {
     amqp.connect(RABBIT_MQ_URL , (error, connection) => {
@@ -16,6 +16,7 @@ export function listenToSalesConfirmationQueue() {
 
             channel.consume(SALES_CONFIRMATION_QUEUE, (message) => {
                 console.info(`Recieve message from queue: ${message.content.toString()}`);
+                OrderService.updateOrder(message);
             }, {
                 noAck: true,
             });

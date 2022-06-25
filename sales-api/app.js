556e9ac0c3d3839039e749dff8de5ac2b537d78f
yhann.carvalho <yhann.carvalho@castgroup.com.br>
2022-06-25 12:06:50 -0300

    data of test

--- sales-api/app.js
+++ sales-api/app.js
@@ -4,6 +4,7 @@ import { connectMongoDB } from "./src/config/db/mongoDbConfig.js";
 import { createInicialData } from './src/config/db/initialData.js';
 import checkToken from './src/config/auth/checkToken.js';
 import { connectRabbitMq } from './src/config/rabbitmq/rabbitConfig.js';
+import { sendMessageToProductStockUpdateQueue } from './src/modules/product/rabbitmq/productStockUpdateSender.js';
  
 const app = express();
 const env = process.env;
@@ -13,7 +14,30 @@ connectMongoDB();
 createInicialData();
 connectRabbitMq();
 
-app.use(checkToken);
+//app.use(checkToken);
+
+app.get('/teste', (req, res) => {
+    try {
+        sendMessageToProductStockUpdateQueue([
+        {
+            productId: 1000,
+            quantity: 3
+        },
+        {
+            productId: 1001,
+            quantity: 2
+        },
+        {
+            productId: 1002,
+            quantity: 1
+        }
+    ])
+        return res.status(200).json({status: 200})
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({err: true})
+    }
+})
 
 app.get('/api/status', async (req, res) => {
     return res.status(200).json({
